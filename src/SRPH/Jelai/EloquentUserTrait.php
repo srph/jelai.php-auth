e216ac5575ca4d8f8dc45b499cc00d0a0fa51e7f
Kier Borromeo <seraphipod@gmail.com>
2015-02-27 10:57:24 +0800

    Added EloquentUserTrait

---
+++ src/SRPH/Jelai/EloquentUserTrait.php
@@ -0,0 +1,25 @@
+<?php namespace SRPH\Jelai\Auth;
+
+trait EloquentUserTrait {
+
+	/**
+	 * Get the unique identifier for the user.
+	 *
+	 * @return mixed
+	 */
+	public function getAuthIdentifier()
+	{
+		return $this->getKey();
+	}
+
+	/**
+	 * Get the password for the user.
+	 *
+	 * @return string
+	 */
+	public function getAuthPassword()
+	{
+		return $this->password;
+	}
+
+}
